34d143e167f833b3c5fc29f1e298a1c1ef40a91a
akhiakter25556 <akhiakterherpower80@gmail.com>
2026-03-08 23:00:13 +0600

    update project

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["i.ibb.co"], // এখানে external image domain add করো
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com", // Google profile photos
+      },
+    ],
   },
 };
 
